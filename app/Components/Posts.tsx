3f22f64d94a3bf076ad4cf7c5faa2714a936a9c7
mousdieng <moizadieng@gmail.com>
2023-10-12 21:33:08 +0000

    fetching data on scroll

--- app/Components/Posts.tsx
+++ app/Components/Posts.tsx
@@ -10,27 +10,37 @@ type PropsParams = {
     ids: number[]
 }
 
-export default function Posts({ids} : PropsParams) {
-    let dataChunked = useMemo(() => chunk(ids, 10), [ids])
-    let [datas, setDatas] = useState<JSX.Element[]>([])
-    let currentPosition = useScroll()
 
+export default function Posts({ ids }: PropsParams) {
+    const dataChunked = useMemo(() => chunk(ids, 10), [ids]);
+    const [postData, setPostData] = useState<{ [key: string]: JSX.Element }>({});
+    const currentPosition = useScroll();
+  
     useEffect(() => {
-        dataChunked[currentPosition].map(async (id) => {
-            let res = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`, {cache: "no-store"})
-            let data: Data = await res.json()
-
-            setDatas(value => [...value, <Post key={id} {...data} />])
-        })
-
-    }, [currentPosition])
-
-
-    return (
-        <>
-            {datas}
-        </>
-    )
+        console.log("hello world!");
+    
+        const fetchData = async () => {
+            const newPostData: { [key: string]: JSX.Element } = {};
+    
+            await Promise.all(
+            dataChunked[currentPosition].map(async (id) => {
+                const res = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`, {
+                cache: "no-store"
+                });
+    
+                const data: Data = await res.json();
+                newPostData[id] = <Post key={id} {...data} />;
+            })
+            );
+    
+            setPostData((prevData) => ({ ...prevData, ...newPostData }));
+        };
+    
+        fetchData();
+    }, [currentPosition, dataChunked]);
+  
+    return <>{Object.values(postData)}</>;
 }
+  
 
 
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
 
     let res = await fetch("https://hacker-news.firebaseio.com/v0/newstories.json", { next: { revalidate: 5}})
     let data: number[] = await res.json()
-
+    
     return (
       <main className='py-24 px-14 space-y-5'>
           <Posts ids={data} />
